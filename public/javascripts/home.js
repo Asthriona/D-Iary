dec1e69dddc37318b0605c90db0d1f82b32821c6
Asthriona <im@asthriona.com>
2019-09-23 17:49:02 +0200

    add the bases

---
+++ public/javascripts/home.js
@@ -0,0 +1,5 @@
+console.log('Hello ＼(≧▽≦)／');
+console.log("I hope you doing great! (≧◡≦)");
+console.log('I having fun talking to you with this console ＼(≧▽≦)／');
+console.log('Anyway I need to keep this thing running! (ノ_<。)	')
+console.log('have fun! (ᵔ.ᵔ)')
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'D-Iary' });
+});
+router.get('/cool', function(req, res, next) {
+  res.render('cool', { title: 'D-Iary - LOL :D' });
 });
 
 module.exports = router;
